422bfcf24537d7930b246975c941edb61bbd3425
MuhsinCanY <96295315+MuhsinCanY@users.noreply.github.com>
2022-06-14 21:31:11 +0300

    Add files via upload

---
+++ UsluSayıHesaplayanProgram.java
@@ -0,0 +1,21 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class UsluSayıHesaplayanProgram {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Base : ");
+        int x = sc.nextInt();
+        System.out.print("Exponent : ");
+        int y = sc.nextInt();
+        int i = 1;
+        int total = 1;
+        for (i=1;i<=y;i++) {
+            total*=x;
+            //System.out.println(total);
+        }
+        System.out.println("Total : "+ total);
+    }
+}
